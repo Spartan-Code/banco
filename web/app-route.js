cb8038e9ae0c98c888f6111be8cbba498888c628
German <you@example.com>
2016-01-07 21:24:45 +0100

    Pequeñas correcciones

--- web/app-route.js
+++ web/app-route.js
@@ -1,8 +1,6 @@
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/main', {
             templateUrl: "main.html",
-            controller: "MainController",
-            css: 'entidadbancaria/css/entidadbancaria-login.css'
             
         });
         $routeProvider.otherwise({
