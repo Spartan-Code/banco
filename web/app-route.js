d8193c7f0321d0f0cfe2f467025d416371801e00
German <you@example.com>
2016-01-07 19:09:22 +0100

    Merge Interfaz

--- web/app-route.js
+++ web/app-route.js
@@ -1,6 +1,7 @@
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/main', {
             templateUrl: "main.html",
+            css: 'entidadbancaria/css/entidadbancaria-login.css'
             
         });
         $routeProvider.otherwise({
